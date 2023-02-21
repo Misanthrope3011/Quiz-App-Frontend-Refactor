b371b7eb3503a0b975d1efde998457c238dd3963
Sebastian Dyjach <sebstian.dyjach@comarch.com>
2023-02-21 18:54:15 +0100

    feat: some css and logic extended

--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -12,13 +12,17 @@ export class SurveyComponent implements OnInit {
 
   content: Question | undefined;
   hideStartPopup = false;
+  routeId: number;
+  answers: string[];
 
   constructor(private requestProcessorService: RequestProcessorService, private activatedRoute: ActivatedRoute, private router: Router) {
       activatedRoute.params.subscribe(err => {
         this.hideStartPopup = true;
         const id = activatedRoute.snapshot.url[activatedRoute.snapshot.url.length - 1].path;
         if(Number(id)) {
-          this.content = this.requestProcessorService.getPayload()[id];
+          this.routeId = Number.parseInt(id);
+          this.content = this.requestProcessorService.getPayload()[this.routeId - 1];
+          console.log(this.routeId)
         } else if(id == "start") {
           this.hideStartPopup = false;
         }
@@ -28,14 +32,30 @@ export class SurveyComponent implements OnInit {
   @HostListener('window:beforeunload', ['$event'])
   unloadNotification($event: any) {
       $event.returnValue = true;
+    this.router.navigate(['']);
+
   }
 
   ngOnInit(): void {
     this.content = this.requestProcessorService.getPayload();
   }
+  selectedAnswerChoiceButton($event: MouseEvent) {
+    $event.stopPropagation(); // add this line
+    this.routeId = this.routeId > 0 ?  this.routeId - 1 : 1;
+    this.router.navigate(['/survey/',  this.routeId]);
+  }
 
-onSubmitSurveyButton() {
+  onClickNextButton($event) {
+    $event.stopPropagation(); // add this line
+    this.routeId = this.routeId + 1;
+   console.log("Clicked" +  this.routeId)
+   this.router.navigate(['/survey/',  this.routeId]);
+  }
 
-}
+  highlightAnswer($event) {
+    const button = $event.target.innerHTML;
+    console.log(button);
+
+  }
 
 }
